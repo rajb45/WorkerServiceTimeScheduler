a6459bf3c9858e8262e256552ad3ff3238886183
rajb45 <rajmbhosale3@gmail.com>
2021-12-06 18:33:55 +0530

    WorkerService

---
+++ WorkerDemoService/Jobs/NotificationJob.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerDemoService.Jobs
+{
+    class NotificationJob : IJob
+    {
+        private readonly ILogger<NotificationJob> _logger;
+        public NotificationJob(ILogger<NotificationJob> logger)
+        {
+            this._logger = logger;
+            
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+
+            var task = Task.Run(() => logfile(DateTime.Now)); ;
+            return task;
+        }
+        public void logfile(DateTime time)
+        {
+            string path = "C:/Users/user/Desktop/Schedule.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(time);
+                writer.Close();
+            }
+        }
+    }
+}
